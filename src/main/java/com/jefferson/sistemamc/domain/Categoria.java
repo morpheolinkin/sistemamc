f3929720bc5f58085fe50875101d34ca042427fa
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2022-03-06 17:05:16 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/jefferson/sistemamc/domain/Categoria.java
+++ src/main/java/com/jefferson/sistemamc/domain/Categoria.java
@@ -4,11 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Categoria {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
-
 }
