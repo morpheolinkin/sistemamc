d38a931b29a259aa3f7763687dde3d6e72e6f0c6
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2022-03-06 16:47:25 -0300

    Testando a primeira classe de domínio

---
+++ src/main/java/com/jefferson/sistemamc/domain/Categoria.java
@@ -0,0 +1,14 @@
+package com.jefferson.sistemamc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    private Integer id;
+    private String nome;
+
+}
--- src/main/java/com/jefferson/sistemamc/resources/CategoriaResource.java
+++ src/main/java/com/jefferson/sistemamc/resources/CategoriaResource.java
@@ -1,16 +1,21 @@
 package com.jefferson.sistemamc.resources;
 
+import com.jefferson.sistemamc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
    @GetMapping
-   public String listar() {
-      return "REST está funcionando!";
-   }
+   public List<Categoria> listar() {
+      Categoria cat1 = new Categoria(1, "Informática");
+      Categoria cat2 = new Categoria(2, "Escritório");
 
+      return List.of(cat1, cat2);
+   }
 }
