3d1349fe07c9d0cd85180b8ef434e2a6531ea2b0
Jefferson M Silva <jeffersonmedeirosdasilva@gmail.com>
2022-03-06 15:58:29 -0300

    Projeto criado

---
+++ src/main/java/com/jefferson/sistemamc/SistemamcApplication.java
@@ -0,0 +1,13 @@
+package com.jefferson.sistemamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemamcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemamcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jefferson/sistemamc/SistemamcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefferson.sistemamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemamcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
